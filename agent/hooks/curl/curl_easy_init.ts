af1492e6e37d44b272d5f297008c5cb5e8447e95
Matthew <matthewtoo5@googlemail.com>
2026-01-26 23:24:04 +0000

    debug curl test

--- agent/hooks/curl/curl_easy_init.ts
+++ agent/hooks/curl/curl_easy_init.ts
@@ -1,13 +1,18 @@
 import { curl } from "../..";
 import { log } from "../../logger";
+import { EnableDebugForCurlHandle } from "../util/EnableDebugForCurlHandle";
+
+
+
 
 export function hook_curl_easy_init() {
-  const curl_easy_init = curl.getExportByName("curl_easy_init");
-
-  Interceptor.attach(curl_easy_init, {
+  const curl_easy_init_ptr = curl.getExportByName("curl_easy_init");
+  Interceptor.attach(curl_easy_init_ptr, {
 
     onLeave(retval) {
       log(`[curl_easy_init] return value: ${retval}`);
+      EnableDebugForCurlHandle(retval)
     },
   });
 }
+
--- agent/index.ts
+++ agent/index.ts
@@ -1,8 +1,9 @@
-import { initialize_hooks } from "./hooks";
-import { hook_all_module_exports } from "./hooks/util/all_exports";
+import { hook_curl_easy_init } from "./hooks/curl/curl_easy_init";
+
+
 
 //Initialize globals
 export const curl = Process.getModuleByName("libcurl.dll"); // Change this depending on the OS you are using!  
 
 // Execute hooks
-initialize_hooks()
+hook_curl_easy_init()
